f954f384aa9ed3f2c8cda0878773a67b3720cfd2
Usman Ashfaq <120087782+Usman-Ashfi@users.noreply.github.com>
2023-11-01 16:40:54 +0500

    home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import lady from "../images/aunti.png";
+import graphic from "../images/graphic.png";
 
 const home = () => {
+  var cards = [1,2,3,4];
   return (
     <div>
       <div className="px-4">
@@ -30,30 +32,44 @@ const home = () => {
           </div>
           {/* Hero Cutomer */}
           <div className="max-w-[1200px] m-auto flex py-10 px-4 justify-evenly  backdrop-blur-[18px] rounded-lg bg-[#00000060]">
-            <div className="flex gap-5 w-1/2 items-center text-md sm:text-lg md:text-xl lg:text-2xl xl:text-4xl 2xl:text-5xl">
+            <div className="flex gap-5  items-center text-md sm:text-lg md:text-xl lg:text-2xl xl:text-4xl 2xl:text-5xl">
               <h3 className="font-[Modren]">Customers</h3>
               <hr className="border h-full transform font-[Modren]" />
               <p>24.3k</p>
             </div>
-            <div className="p-3 w-1/2">hi</div>
           </div>
         </section>
         {/* Service */}
-        <section className="neon animation">
-          <div className="grid grid-rows-2 md:grid-cols-2 md:grid-rows-1 gap-2 max-w-[1200px] m-auto my-5">
+        <section className="neon animation md:py-10">
+          <div className="flex flex-col-reverse md:grid md:grid-flow-col-dense md:grid-rows-1 gap-6 max-w-[1200px] m-auto my-5">
             {/* Cards */}
-            <div className=" max-h-[400px] w-full">
-              <script
-                type="module"
-                src="https://unpkg.com/@splinetool/viewer@0.9.490/build/spline-viewer.js"
-              ></script>
-              <spline-viewer url="https://prod.spline.design/QQz8A5HmzyWfuLch/scene.splinecode"></spline-viewer>
+            <div className="flex flex-wrap gap-4 w-full">
+              {cards.map(()=>(
+
+              <div className="relative w-[290px] h-[200px] backdrop-blur-3xl rounded-lg bg-[#ffffff11] shadow-2xl">
+                <div className="w-full h-[100%]">
+                  <Image
+                    alt=""
+                    className="w-full h-full object-contain"
+                    src={graphic}
+                  ></Image>
+                </div>
+                <div className="absolute z-50 bottom-2 w-full  px-2 ">
+                  <div className="flex justify-between items-center p-1 rounded-lg backdrop-blur-xl bg-[#00000084] border border-[#0000001e]">
+                  <h2 className="text-xs">Graphic Design</h2>
+                  <button className="text-xs px-2 py-1 rounded-lg">
+                    Learn More
+                  </button>
+                  </div>
+                </div>
+              </div>
+              ))}
             </div>
             {/* Services info */}
-            <div className="px-10">
-              <div className="flex flex-col gap-10 justify-center">
+            <div className="md:px-10">
+              <div className="flex flex-col gap-5 justify-center">
                 <span className="text-orange-400">Popular Services ---→</span>
-                <h2 className="text-md sm:text-lg md:text-xl lg:text-2xl xl:text-4xl 2xl:text-5xl font-bold font-[Modren]">
+                <h2 className="text-4xl  font-bold font-[Modren]">
                   Hot Trending <br /> Services
                 </h2>
                 <p>
