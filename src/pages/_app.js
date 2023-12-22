000a3c341d7049db9bdfbae715fe1e6ed5dac0f9
Usman Ashfaq <120087782+Usman-Ashfi@users.noreply.github.com>
2023-12-22 16:08:07 +0500

    speed insight

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,9 @@
-import "@/styles/globals.css";
 import "@/styles/form.css";
+import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "react-toastify/dist/ReactToastify.css";
 import Layout from "./layout";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }) {
     <Layout>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
+        <SpeedInsights />
       </QueryClientProvider>
     </Layout>
   );
